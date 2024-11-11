1ebee1343c1ef006397c7db9fb8b414604957f4f
unknown <abhideep.2004.3@gmail.com>
2024-11-11 13:18:31 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,5 +2,10 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
+        if not grid or not grid[0]:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    visited = [[False for _ in range(cols)] for _ in range(rows)]
                     
         return 0
