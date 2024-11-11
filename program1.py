73e224cb455b9259f17a8630d28262c107963ef7
unknown <abhideep.2004.3@gmail.com>
2024-11-11 13:23:59 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -3,7 +3,7 @@ class Solution:
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
         if not grid or not grid[0]:
-            print(0)
+            return 0
 
         rows, cols = len(grid), len(grid[0])
         visited = [[False for _ in range(cols)] for _ in range(rows)]
@@ -24,4 +24,4 @@ class Solution:
                     dfs(r, c)
                     island_count += 1
 
-        print(island_count)
+        return island_count
